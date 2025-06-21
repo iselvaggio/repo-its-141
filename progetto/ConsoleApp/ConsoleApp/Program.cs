b0de533e60b50251c0ffee56c0eb53cec7dd4e3c
IgnazioSelvaggio <49954442+IgnazioSelvaggio@users.noreply.github.com>
2025-06-21 15:49:25 +0200

    1.test

--- progetto/ConsoleApp/ConsoleApp/Program.cs
+++ progetto/ConsoleApp/ConsoleApp/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            // Check if the user provided a name as an argument
         }
     }
 }
