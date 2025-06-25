0a3759d0d30426ff1d05427cc357c6835d8de6fe
IgnazioSelvaggio <49954442+IgnazioSelvaggio@users.noreply.github.com>
2025-06-25 08:31:11 +0200

    1. commit OK

--- progetto/ConsoleApp/TestProject1/Test1.cs
+++ progetto/ConsoleApp/TestProject1/Test1.cs
@@ -6,8 +6,8 @@
         [TestMethod]
         public void TestMethod1()
         {
-            //fail
-            Assert.Fail("This test has not been implemented yet.");
+            Assert.AreEqual(1, 1);
+
         }
     }
 }
