d255120c869b3590bb22781174b0b3531a530501
IgnazioSelvaggio <49954442+IgnazioSelvaggio@users.noreply.github.com>
2025-06-26 16:26:48 +0200

    #1 commit test

---
+++ progetto/ConsoleApp/LibCalcolatrice/Calcolatrice.cs
@@ -0,0 +1,10 @@
+﻿namespace LibCalcolatrice
+{
+    public class Calcolatrice
+    {
+        public int Somma(int addendo1, int addendo2)
+        {
+            return addendo1 + addendo2;
+        }
+    }
+}
---
+++ progetto/ConsoleApp/TestCalcolatrice/Test1.cs
@@ -0,0 +1,16 @@
+﻿using LibCalcolatrice;
+
+namespace TestCalcolatrice
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSomma()
+        {
+            var calc = new Calcolatrice();
+            var risultato = calc.Somma(3, 5);
+            Assert.AreEqual(7, risultato);
+        }
+    }
+}
